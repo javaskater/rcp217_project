daedfc460594069ccd54eeedb529e916e74da17f
Jean-Pierre MENA <jpm@jpmena.eu>
2025-07-19 17:10:37 +0200

    Time serie generation working

--- R/testGenerateARMASeries_methode2.R
+++ R/testGenerateARMASeries_methode2.R
@@ -1,27 +1,43 @@
-
-stationary_time_serie <- True
-tryCatch(                
-  expr = {                      
-    ts.sim4 <- arima.sim(n=200, list(ar = ar_coeffs, ma = ma_coeffs), rand.gen = function(n, ...) sqrt(0.1796) * rt(n, df = 5))
-    ar1 <- arima.sim(model = list(order = c(4,0,0), ar = ar_coeffs), n = 200)
-    stationary_time_serie <- TRUE
-    print("Everything was fine. the serie is stationary I Plot")
-    ts.plot(ts.sim4)
-  },
-  error = function(e){
-    stationary_time_serie <<- FALSE
-    message("There was an error message.", e)
-  },
-  
-  warning = function(w){       
-    stationary_time_serie <<- FALSE
-    message("There was a warning message.", w)
-  },
-  
-  finally = {             
-    print("finally Executed")
+calculate_times_serie <- function(p, q){
+  stationary_time_serie <- FALSE
+  time_serie <- c()
+  ar_coeffs <- c()
+  ma_coeffs <- c()
+  while (!stationary_time_serie){
+    tryCatch(                
+      expr = { 
+        ar_coeffs <- runif(p,0,1) # generates order number of coeffcients between 0 and 1
+        ma_coeffs <- runif(q,0,1) # generates order number of coeffcients between 0 and 1
+        msg_str = sprintf("[calculate_times_serie] checking ar_coeffs: %s with ma_coeffs: %s", paste0(ar_coeffs, collapse = "|"), paste0(ma_coeffs, collapse = "|"))
+        print(msg_str)
+        #time_serie <<- arima.sim(model = list(order = c(p,0,q), ar = ar_coeffs, ma = ma_coeffs), n = 200)
+        time_serie <- arima.sim(model = list(ar = ar_coeffs, ma = ma_coeffs), n = 200)
+        # if we pass the previous expression we have a stationary time serie
+        stationary_time_serie <<- TRUE
+        msg_str = sprintf("[calculate_times_serie] Everything was fine. the serie is stationary")
+        print(msg_str)
+        break
+      },
+      error = function(e){
+        stationary_time_serie <<- FALSE
+        msg_str = sprintf("[calculate_times_serie] error ar ma coefficients not stationary. Message d'erreur:%s", e$message)
+        print(msg_str)
+      },
+      
+      warning = function(w){       
+        stationary_time_serie <<- FALSE
+        msg_str = sprintf("[calculate_times_serie] Message de warning %s", w$message)
+        print(msg_str)
+      },
+      
+      finally = {             
+        print("[calculate_times_serie] + finally Executed")
+      }
+    )
   }
-)
-# test a  ar1 <- arima.sim(model = list(order = c(4,0,0), ar = ar_coeffs), n = 200)
-ar1 <- arima.sim(model = list(order = c(4,0,0), ar = ar_coeffs), n = 200)
-# followed by ts.plot(ar1)
+  return (list(ar_coefficients <- ar_coeffs, ma_coefficients <- ma_coeffs, arma_timeserie <- time_serie))
+}
+
+my_ts <- calculate_times_serie(4,5)
+arma1 = my_ts[[3]]
+ts.plot(arma1)
--- docs/GENERATION/R.md
+++ docs/GENERATION/R.md
@@ -109,4 +109,56 @@ arima.sim(model = list(order = c(4, 0, 0), ar = ar_coeffs), n = 200)
 cf. [test général des ARMA Series](../../R/testARMASeries.R)
 * C'est l'objet de [test de Génération Méthode 2](../../R/testGenerateARMASeries_methode2.R)
  * retourner un dict contenant le ar et ma coefficient ainsi que la time serie (todo)
- * voir comment on obtient les points de la time serie (todo)
+ * Les listes R sont mes amies
+```R
+y <- LETTERS[1:10]
+> y
+ [1] "A" "B" "C" "D" "E" "F" "G" "H" "I" "J"
+> x <- 1:3
+> x
+[1] 1 2 3
+> my_list <- list(coeff=x,data=y) # cf. Python dictionaries
+> my_list[['coeff']] # Pour le premier niveau utiliser les doubles brackets
+[1] 1 2 3 # c'est un vector
+> my_list[['coeff']][2]
+[1] 2 # c'est un vecteur unitaire ou un entier
+> my_list[['coeff']][[2]]
+[1] 2 # même résultat que précédemment
+```
+# Conclusion ce samedi 19/07/2025 à 17h
+* Le code fonctionne
+```R
+my_ts <- calculate_times_serie(4,5)
+arma1 = my_ts[[3]]
+ts.plot(arma1)
+```
+## NB 
+* dans arima.sim le order=c(p,0,q) ne fonctionne pas, on donne directement les ar et les ma coefficients
+* Pour accéder aux différentes parties de la liste dans la console et le code R passer par les indices (qui commencent à 1) et non par les entrées du dictionnaire
+```R
+> my_ts[['ar_coefficients']]
+NULL
+> my_ts[[1]]
+[1] 0.1092415 0.3497106 0.2525748 0.1406372
+> my_ts[[2]]
+[1] 0.7967161 0.6926423 0.1887584 0.9610122 0.6902769
+> my_ts[[3]]
+Time Series:
+Start = 1 
+End = 200 
+Frequency = 1 
+  [1]   7.45438713   6.16172410   6.17463007   6.05140955   5.57350931   1.85552825   1.83457581   2.67188012   1.83783567   1.18452208
+ [11]   0.17797997   0.97789176   0.99913365   1.41919738  -0.41166497  -0.77101924   2.44175196  -0.71287187   0.54371471   0.01098280
+ [21]   0.79571980  -0.81205788  -1.51658554  -1.36912269  -3.85088685  -2.56064349  -3.23340994  -3.50781488  -5.41202106  -5.65746315
+ ## Et ainsi jusqu'à 200 valeurs
+```
+* voir comment on obtient les points de la time serie
+```R
+> arma1[[1]]
+[1] 7.454387 # on obtient vraiment le premier élément, on a juste un problème de précision
+> arma1[1] # double ou simple donne le même résultat
+[1] 7.454387
+> length(arma1) # se comporte comme un vecteur
+[1] 200
+```
+## car mettre ces données dans un fichier CSV qui sera nommé avec p et q et un indice (todo)
